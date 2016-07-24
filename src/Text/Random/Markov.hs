c59f8c4d78c78525491392ef82f5745955706a2f
kmein <kieran.meinhardt@gmail.com>
2016-07-24 21:58:41 +0200

    Fix Vector imports and scoping

--- src/Text/Random/Markov.hs
+++ src/Text/Random/Markov.hs
@@ -2,53 +2,52 @@ module Text.Random.Markov where
 
 import Control.Arrow (first)
 import Data.Char (isUpper)
-import Data.Vector
+import Data.Vector (Vector, (!))
+import qualified Data.Vector as V
 import Data.Maybe (fromMaybe)
 import qualified Data.Map.Strict as M ((!), Map, empty, insertWith)
 import qualified Data.Text as T (Text, head, unwords, words)
 import qualified Data.Text.IO as T (readFile)
-import Prelude hiding ((++), filter, length, map)
 import System.Random (RandomGen, randomR, newStdGen)
 
 type TextWord = T.Text
 type TextText = Vector TextWord
 type TextCache = M.Map (TextWord, TextWord) TextText
 
+choice :: (RandomGen g) => g -> Vector a -> (a, g)
+choice g xs = (xs !) `first` randomR (0, V.length xs - 1) g
+
 startWords :: TextText -> TextText
-startWords = filter (isUpper . T.head)
+startWords = V.filter (isUpper . T.head)
 
 triplets :: TextText -> Vector (TextWord, TextWord, TextWord)
 triplets wrds =
     if size < 3
-       then empty
-       else flip map (fromList [0 .. (size - 3)]) $ \i ->
+       then V.empty
+       else flip fmap (V.fromList [0 .. (size - 3)]) $ \i ->
            (wrds ! i, wrds ! (i+1), wrds ! (i+2))
     where
-      size = length wrds
+      size = V.length wrds
 
 createStatistics :: TextText -> TextCache
 createStatistics =
-    foldl' (\acc (w1, w2, w3) -> M.insertWith (++) (w1, w2) (singleton w3) acc)
+    V.foldl' (\acc (w1, w2, w3) -> M.insertWith (V.++) (w1, w2) (V.singleton w3) acc)
         M.empty . triplets
 
 generateMarkovText :: (RandomGen g) => TextText -> g -> Int -> T.Text
 generateMarkovText wrds gen size =
-    T.unwords $ toList $ generate seedWord nextWord gen' size
+    T.unwords $ V.toList $ generate seedWord nextWord gen' size
     where
       (seedStartWord, gen') = choice gen $ startWords wrds
-      seed = fromMaybe 0 $ elemIndex seedStartWord wrds
-
+      seed = fromMaybe 0 $ V.elemIndex seedStartWord wrds
       seedWord = wrds ! seed
       nextWord = wrds ! (seed + 1)
-
-      choice g xs = (xs !) `first` randomR (0, length xs - 1) g
-
-      generate = go empty
+      generate = go V.empty
           where
             go acc w1 w2 g n =
                 case n of
-                  0 -> acc `snoc` w2
-                  _ -> go (acc `snoc` w1) w2 w' g' (n - 1)
+                  0 -> acc `V.snoc` w2
+                  _ -> go (acc `V.snoc` w1) w2 w' g' (n - 1)
                 where
                   (w', g') = choice g (cache M.! (w1, w2))
                   cache = createStatistics wrds
@@ -56,6 +55,6 @@ generateMarkovText wrds gen size =
 textGen :: Int -> FilePath -> IO T.Text
 textGen len file =
     generateMarkovText
-    <$> (fromList <$> T.words <$> T.readFile file)
+    <$> (V.fromList <$> T.words <$> T.readFile file)
     <*> newStdGen
     <*> pure len
